47eba2374fd1ea736507f816aa58995bb3a81ee6
prothesbarai <aspprothesbarai1619@gmail.com>
2024-12-09 22:17:54 +0600

    v.2

--- Practices_Alll.java
+++ Practices_Alll.java
@@ -1,8 +1,27 @@
 package com.mycompany.basicjava;
-import java.util.Random;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Date;
 
 public class Practices_Alll {
     public static void main(String[] args) {
-        Random random = new Random();
+        
+        Date date = new Date();
+        System.out.println(date);
+        
+        DateFormat dateFormat = new SimpleDateFormat("dd/MM/YYYY");
+        String sd = dateFormat.format(date);
+        System.out.println(sd);
+        
+        LocalTime localTime = LocalTime.now();
+        System.out.println(localTime);
+        DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("hh:mm:ss");
+        String ff = localTime.format(dateTimeFormatter);
+        System.out.println(ff);
+        
+        
     }
 }
