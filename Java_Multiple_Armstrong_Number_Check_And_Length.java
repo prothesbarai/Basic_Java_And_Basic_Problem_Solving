7b2efed945ad8a9b3e07d3790eb57fe1b60de973
prothesbarai <aspprothesbarai1619@gmail.com>
2024-12-09 20:16:32 +0600

    Armstrong Number V.2

--- Java_Multiple_Armstrong_Number_Check_And_Length.java
+++ Java_Multiple_Armstrong_Number_Check_And_Length.java
@@ -1,13 +1,53 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package com.mycompany.basicjava;
 
-/**
- *
- * @author Asp Prothes
- */
+import java.util.Scanner;
+
 public class Java_Multiple_Armstrong_Number_Check_And_Length {
+    public static void main(String[] args) {
+        /*
+            Armstrong Example = 153
+            1^3 + 5^3 + 3^3 = 153
+        */
+        int num1, num2, count = 0;
+        Scanner scanner = new Scanner(System.in);
+        
+        System.out.print("Input 1st Integer Number : ");
+        num1 = scanner.nextInt();
+        
+        System.out.print("Input 2nd Integer Number : ");
+        num2 = scanner.nextInt();
+
+        if (num1<num2) {
+            for (int i = num1; i <= num2; i++) {
+                if (isArmstrong(i)) {
+                    count++;
+                    System.out.print(i+" , ");
+                }
+            }
+        }else{
+            System.out.println("Must Be, 2nd Number Big than 1st Number"); 
+        }
+        
+        System.out.println("\nTotal Length = "+count);
+        
+        
+    }
+    
+    public static boolean isArmstrong(int num){
+        int remainder,sum = 0,store,temp;
+        temp = num;
+        while(temp != 0){
+            remainder = temp % 10;
+            store = remainder * remainder * remainder;
+            sum += store;
+            temp = temp / 10;
+        }
+        
+        if (num == sum) {
+            return true;
+        }
+        
+        return false;
+    }
     
 }
