473dd8b3056528dd9453d236bf71faf3f984e3ac
prothesbarai <aspprothesbarai1619@gmail.com>
2024-12-12 11:37:11 +0600

    String Regex Concept v1

--- Practices_Alll.java
+++ Practices_Alll.java
@@ -1,30 +1,21 @@
 package com.mycompany.basicjava;
+
 import java.util.Scanner;
+
 public class Practices_Alll {
-       public static void main(String[] args) {
+
+    public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        int n = scanner.nextInt();  // Read the number of usernames
-        scanner.nextLine();         // Consume the newline
-
-        for (int i = 0; i < n; i++) {
-            String username = scanner.nextLine();
-            if (isValidUsername(username)) {
-                System.out.println("Valid");
-            } else {
-                System.out.println("Invalid");
-            }
+        String n = scanner.nextLine();
+        
+        String regex = "a...b";
+        
+        if (n.matches(regex)) {
+            System.out.println("Match");
+        }else{
+            System.out.println("No Match");
         }
-
-        scanner.close();
-    }
-
-    public static boolean isValidUsername(String username) {
-        String regex = "^[a-zA-Z][a-zA-Z0-9_]*$";
-        if (username.matches(regex) && username.length()>=8 && username.length()<=30) {
-            return true;
-        }
-
-        return false;
+        
     }
 }
