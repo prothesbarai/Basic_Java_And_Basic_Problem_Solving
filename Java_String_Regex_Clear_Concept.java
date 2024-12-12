058e445fb5df8186f9611e21c4111b3b929a1498
prothesbarai <aspprothesbarai1619@gmail.com>
2024-12-12 15:17:43 +0600

    v3

--- Java_String_Regex_Clear_Concept.java
+++ Java_String_Regex_Clear_Concept.java
@@ -2,19 +2,18 @@ package com.mycompany.basicjava;
 import java.util.Scanner;
 public class Java_String_Regex_Clear_Concept {
     public static void main(String[] args) {
-        String getString1,regex1,regex2,regex3,regex4,regex5,regex6,regex7,regex8,regex9;
+        String regex1,regex2,regex3,regex4,regex5,regex6,regex7,regex8,regex9;
+        String regex10,regex11,regex12,regex13,regex14,regex15;
         Scanner scanner = new Scanner(System.in);
         
         /* ==== Define Regex Formula ==== */
      
         
-        System.out.print("Input For Regex1 : (a..b) : ");
-        getString1 = scanner.nextLine();
-        
-
+       
         // Type 1 :  "a.c" → "arc" or "a..c" → "arrc", or "a...c" → "arrrc"
         regex1 = "a..b"; // First a Last b but middle only 2 another
-        
+        System.out.print("Input For Regex1 : (a..b) : ");
+        String getString1 = scanner.nextLine();
         if (getString1.matches(regex1)) {
             System.out.println("Match Regex 1 : formula : a..b");
         }else{
@@ -67,6 +66,52 @@ public class Java_String_Regex_Clear_Concept {
         }else{
             System.out.println("No Match. soln: Anywhere must be abc");
         }
+        
+        
+        // Type 6 :  "[abc]" [ Any character within the string can be matched a , b or c ]
+        regex6 = "[abc]"; // only one character match at the same time .. only one input
+        System.out.print("\nInput For Regex6 : [abc] : ");
+        String getString6 = scanner.nextLine();
+        if (getString6.matches(regex6)) {
+            System.out.println("Match Regex 6 : formula : [abc]");
+        }else{
+            System.out.println("No Match. soln: a or b or c character in one Input");
+        }
+        
+        
+        // Type 7 :  "[^abc]" [ Without a,b or c match any Character ]
+        regex7 = "[^abc]"; // without abc input any single character
+        System.out.print("\nInput For Regex7 : [^abc] : ");
+        String getString7 = scanner.nextLine();
+        if (getString7.matches(regex7)) {
+            System.out.println("Match Regex 7 : formula : [^abc]");
+        }else{
+            System.out.println("No Match. soln: Without a,b or c match any single character");
+        }
+        
+        
+        // Type 8 :  "[0-9]" [ 0 to 9 any single value match ]
+        regex8 = "[0-9]"; // Only Single value match
+        System.out.print("\nInput For Regex8 : [0-9] : ");
+        String getString8 = scanner.nextLine();
+        if (getString8.matches(regex8)) {
+            System.out.println("Match Regex 8 : formula : [0-9]");
+        }else{
+            System.out.println("No Match. soln: Only Value Match 0 to 9 Single Value");
+        }
+        
+        
+        // Type 9 :  "[a-z]" [ a to z any single Character match ]
+                            // Same as [A-Z] for Capital Letter
+        regex9 = "[a-z]"; // Only Single Character match
+        System.out.print("\nInput For Regex9 : [a-z] : ");
+        String getString9 = scanner.nextLine();
+        if (getString9.matches(regex9)) {
+            System.out.println("Match Regex 9 : formula : [a-z]");
+        }else{
+            System.out.println("No Match. soln: a to z any single Character match");
+        }
+        
         
         
     }
--- Practices_Alll.java
+++ Practices_Alll.java
@@ -5,17 +5,18 @@ import java.util.Scanner;
 public class Practices_Alll {
 
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-
-        String n = scanner.nextLine();
         
-        String regex = "a...b";
         
-        if (n.matches(regex)) {
-            System.out.println("Match");
-        }else{
-            System.out.println("No Match");
-        }
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
         
     }
 }
