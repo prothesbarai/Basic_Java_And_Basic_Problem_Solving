abd642247371f88f15d2b3ed2a5c3bad46f73da0
prothesbarai <aspprothesbarai1619@gmail.com>
2024-12-13 20:08:16 +0600

    One Dimentional Array P1

--- Java_Array_1_Dimentional_Part_1.java
+++ Java_Array_1_Dimentional_Part_1.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 
 public class Java_Array_1_Dimentional_Part_1 {
     public static void main(String[] args) {
-        int num,sum=0;
+        int num,sum=0,max,min;
         double avg,arrayLength;
         
         Scanner scanner = new Scanner(System.in);
@@ -32,8 +32,32 @@ public class Java_Array_1_Dimentional_Part_1 {
             sum += myArray[i]; 
         }
         System.out.println("\nSum Of Array = "+sum);
+        
+        // Avearge Array..
         arrayLength = myArray.length;
         avg = sum/arrayLength;
         System.out.println("Avg : "+avg);
+        
+        // Find the max number in array
+        max = myArray[0]; // Let, Initially Index 0 position is big 
+        for (int i = 1; i < myArray.length; i++) {
+            if (max<myArray[i]) {
+                max = myArray[i];
+            }
+        }
+        System.out.println("Maximum = "+max);
+        
+        
+        // Find the min number in array
+        min = myArray[0];
+        for (int i = 1; i < myArray.length; i++) {
+            if (min>myArray[i]) {
+                min = myArray[i];
+            }
+        }
+        System.out.println("Minimum = "+min);
+        
+        
+        
     }
 }
