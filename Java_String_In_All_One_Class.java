960fddacd4b7cd436d5bca279a9bee12dbc6ea6c
prothesbarai <aspprothesbarai1619@gmail.com>
2024-12-15 14:02:36 +0600

    s2

--- Java_String_In_All_One_Class.java
+++ Java_String_In_All_One_Class.java
@@ -1,7 +1,7 @@
 package com.mycompany.basicjava;
 public class Java_String_In_All_One_Class {
     public static void main(String[] args) {
-        String s1 = "prothes",s2 = "Prothes";
+        String s1 = "prothes",s2 = "Prothes",s3 = "Barai";
         
         // Length Of String
         System.out.println("Length s1 = "+s1.length());
@@ -24,6 +24,17 @@ public class Java_String_In_All_One_Class {
         }
         
         
+        // Concat Type two way 1. method 2. + operator
+        System.out.println(s2+" "+s3);
+        System.out.println(s2.concat(" "+s3));
+        
+        
+        
+        // Lowercase to uppercase
+        System.out.println(s1.toUpperCase());
+        
+        // Uppercase to lowercase
+        System.out.println(s2.toLowerCase());
         
     }
 }
