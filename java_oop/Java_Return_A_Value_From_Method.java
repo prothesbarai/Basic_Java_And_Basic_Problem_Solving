35e1a7a5753762e9b0de653b8761d0421767beac
prothesbarai <aspprothesbarai1619@gmail.com>
2024-12-18 10:32:51 +0600

    Return Type Concept

--- java_oop/Java_Return_A_Value_From_Method.java
+++ java_oop/Java_Return_A_Value_From_Method.java
@@ -2,7 +2,7 @@ package com.mycompany.basicjava.java_oop;
 import java.util.Scanner;
 public class Java_Return_A_Value_From_Method {
     public static void main(String[] args) {
-        int num,phnNumerCheck;
+        int num,phnNumerCheck,voter_age;
         Scanner scanner = new Scanner(System.in);
         System.out.print("Input An Number : ");
         num = scanner.nextInt();
@@ -10,20 +10,34 @@ public class Java_Return_A_Value_From_Method {
         System.out.print("Input An Country Phone Number Code : ");
         phnNumerCheck = scanner.nextInt();
         String phnNumber = Integer.toString(phnNumerCheck);
+        
+        System.out.print("Input Your Age : ");
+        voter_age = scanner.nextInt();
 
         Mymath mymath = new Mymath();
         
+        // For Int Return Type
         int result1 = mymath.Square(num);
         System.out.println(num+" : Square Result is : "+result1);
+        
+        // For String Return Type
         String result2 = mymath.CheckPhnNumber(phnNumber);
         System.out.println(phnNumber+" : This Number Codes of Country Name is : "+result2);
         
+        // For Boolean Return Type
+        boolean result3 = mymath.isTrue(voter_age);
+        if (result3) {
+            System.out.println("Your age is : "+voter_age+" So you are eligible for vote.");
+        }else{
+            System.out.println("Your age is : "+voter_age+" You are not eligible for vote.");
+        }
     }
     
     
     // Create User Define Class
     public static class Mymath{
         
+        // Integer Return Type same as float and Double
         public int Square(int num){
             int x;
             x = num*num;
@@ -31,6 +45,7 @@ public class Java_Return_A_Value_From_Method {
         }
         
         
+        // String Return Type
         public String CheckPhnNumber(String countryCode){
             String result;
             if(countryCode.contains("1")){
@@ -47,6 +62,15 @@ public class Java_Return_A_Value_From_Method {
                 result = "Now i am Small Method, I Dont Know the Code";
             }
             return result;
+        }
+        
+        
+        // Boolean Return Type
+        public boolean isTrue(int age){
+            if (age>=18) {
+                return true;
+            }
+            return false;
         }
         
         
