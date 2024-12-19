a90ffa5600a5845743bfc2ab9282e49efd6b8a31
prothesbarai <aspprothesbarai1619@gmail.com>
2024-12-19 22:14:54 +0600

    main

--- basicjava/main.java
+++ basicjava/main.java
@@ -2,8 +2,48 @@ package com.mycompany.basicjava;
 public class main {
     public static void main(String[] args) {
         
+        int a=7,b=7;
+        float c=7.7f,d;
+        d=a+b+c;
+        System.out.println("Result"+d);
+        
+        
+        
+        String x="Prothes is a good boy";
+        System.out.println("Who is Prothes?\n"+x);
+        
+        
+        
+        int aa=7;
+        float bb=12.30f;
+        double cc=123.4673992,ee;
+        String dd="USD";
+        ee=aa*bb*cc;
+        
+        System.out.println(dd+" : "+ee);
+        
+        
+        
+        double ab=11.123,ae;
+        int ac=122;
+        float ad=1.32f;
+        ae=ab/ac/ad;
+        
+        System.out.println(ae);
+        
+        
+        
+        int aaa=12345, eee=123984678;
+        double bbb=1234.567890,ddd;
+        float ccc=198.98f;
+        ddd=aaa*bbb+ccc-eee;
+        
+        System.out.println(ddd);
+        
+        
+        
+        
+        
+                
     }
-    
-    
-    
 }
