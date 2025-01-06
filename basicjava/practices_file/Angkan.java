e833967da73ad0496e77bddeebd1b23fabdc711b
prothesbarai <aspprothesbarai1619@gmail.com>
2025-01-06 11:34:14 +0600

    p5

--- basicjava/practices_file/Angkan.java
+++ basicjava/practices_file/Angkan.java
@@ -1,4 +1,5 @@
 package com.mycompany.basicjava.practices_file;
+
 public class Angkan {
     
 }
--- basicjava/practices_file/Pro.java
+++ basicjava/practices_file/Pro.java
@@ -5,12 +5,54 @@ import java.util.Scanner;
 public class Pro {
     public static void main(String[] args) {
         
-       double a = 33.556;
-       double b = 55.33;
-       double x = Math.PI;
-       double y = Math.floor(a);
-       double z = Math.ceil(a);
-       System.out.println(""+x+"\n"+y+"\n"+z);
+        int num,sum=0,max,min;
+        Scanner scanner = new Scanner(System.in);
+        // Input Array Size
+        num = scanner.nextInt();
+        scanner.nextLine();
+        
+        // Array Creation And Declaration
+        int[] myArray = new int[num];
+        // Input An Array
+        for (int i = 0; i < num; i++) {
+            myArray[i] = scanner.nextInt();
+        }
+        
+        System.out.println("\n\n\n");
+        // Output
+        for (int i = 0; i < myArray.length; i++) {
+            System.out.println(""+myArray[i]);
+        }
+        // Sum Of Array
+        for (int i = 0; i < myArray.length; i++) {
+            sum += myArray[i];
+        }
+        System.out.println("Sum : "+sum);
+        // Max
+        max = myArray[0];
+        for (int i = 0; i < myArray.length; i++) {
+            if (max < myArray[i]) {
+                max = myArray[i];
+            }
+        }
+        System.out.println("Max : "+max);
+        
+        // Minimum Number
+        min = myArray[0];
+        for (int i = 0; i < myArray.length; i++) {
+            if (min > myArray[i]) {
+                min = myArray[i];
+            }
+        }
+        System.out.println("Min : "+min);
+        
+        // Avg
+        double avg;
+        avg = sum/myArray.length;
+        System.out.println("Avg : "+avg);
+        
+        
+        
         
     }
 }
