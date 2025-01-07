b5efa407600385ecc2bbf35cebdb05c2e1c8b099
prothesbarai <aspprothesbarai1619@gmail.com>
2025-01-07 12:43:31 +0600

    oo

--- basicjava/practices_file/RunnerFile.java
+++ basicjava/practices_file/RunnerFile.java
@@ -3,9 +3,11 @@ import java.util.Arrays;
 import java.util.Scanner;
 public class RunnerFile {
     public static void main(String[] args) {
-        
-        
 
+        Students student_marks = new Students(5,5,4,5,3.17);
+        double gpa = student_marks.calculateGpa();
+        Students students = new Students("Prothes", "Male", 25,"Science", "CSE", 18502002591L, 1923, 2025,gpa);
+        students.setPrinted();
         
         
         
--- basicjava/practices_file/Students.java
+++ basicjava/practices_file/Students.java
@@ -1,13 +1,61 @@
 package com.mycompany.basicjava.practices_file;
 
+import java.util.ArrayList;
+
 public class Students {
-    String names,group,sub;
-    int roll,year,id;
-    double gpa;
+    public String names,group,sub,gender;
+    public int id,year,age;
+    public long roll;
+    public double gpa;
+    public double[] gradePoints;
+   
     
-    public Students(){
-        
+    public Students(String names,String gender,int age,String group,String sub,long roll,int id,int year,double gpa){
+        this.names = names;
+        this.gender = gender;
+        this.age = age;
+        this.group = group;
+        this.sub = sub;
+        this.roll = roll;
+        this.id = id;
+        this.year = year;
+        this.gpa = gpa;
     }
     
+    public Students(double ... gradePoints){
+        this.gradePoints = gradePoints;
+    }
+    
+    
+    public double calculateGpa(){
+        double sum = 0,result,creditHours = 3.0,temp,totalCreditHours ;
+        ArrayList<Double> arrayList = new ArrayList();
+        for(double i : gradePoints) {
+            temp = i * creditHours;
+            arrayList.add(temp);
+        }
+        for (double i : arrayList) {
+            sum += i;
+        }
+        
+        totalCreditHours = 3.0 * arrayList.size();
+        result = sum / totalCreditHours;
+        
+        return result;
+    }
+    
+    
+    public void setPrinted(){
+        System.out.println("Name : "+names);
+        System.out.println("Gender : "+gender);
+        System.out.println("Age : "+age);
+        System.out.println("Group : "+group);
+        System.out.println("Subject : "+sub);
+        System.out.println("Roll : "+roll);
+        System.out.println("ID : "+id);
+        System.out.println("Year : "+year);
+        System.out.println("GPA : " + gpa);
+
+    }
     
 }
