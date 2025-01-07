0e76ba1ed15cdcc863ab549dfd32586c5908d13a
prothesbarai <aspprothesbarai1619@gmail.com>
2025-01-07 10:20:22 +0600

    p4

--- basicjava/practices_file/Pro.java
+++ basicjava/practices_file/Pro.java
@@ -1,14 +1,17 @@
 package com.mycompany.basicjava.practices_file;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Pro {
     public static void main(String[] args) {
         
-        int num,sum=0,max,min;
+        int num,sum=0,max,min,dayOfNum;
+        String[] weeksDay = {"Friday","Saturday","Sunday","Monday"};
         Scanner scanner = new Scanner(System.in);
         // Input Array Size
         num = scanner.nextInt();
+        dayOfNum = scanner.nextInt();
         scanner.nextLine();
         
         // Array Creation And Declaration
@@ -52,6 +55,24 @@ public class Pro {
         System.out.println("Avg : "+avg);
         
         
+        if (dayOfNum >= 0 && dayOfNum < 7) {
+            String nameOfDay = weeksDay[dayOfNum];
+            System.out.println(""+nameOfDay);
+        }else{
+            System.out.println("Empty");
+        }
+        
+        
+        Arrays.sort(myArray);
+        for (int i = 0; i < myArray.length; i++) {
+            System.out.print(myArray[i]+" ");
+        }
+        
+        System.out.println("\n");
+        
+        for (int i = myArray.length-1; i >= 0; i--) {
+            System.out.print(myArray[i]+" ");
+        }
         
         
     }
