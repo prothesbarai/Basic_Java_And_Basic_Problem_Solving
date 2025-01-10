1b530b0005113e1ca490a63a443b9c94b5ca30e6
prothesbarai <aspprothesbarai1619@gmail.com>
2025-01-10 21:39:01 +0600

    P0

--- basicjava/this_keyword/RunnerClass.java
+++ basicjava/this_keyword/RunnerClass.java
@@ -1,6 +1,31 @@
 package com.mycompany.basicjava.this_keyword;
+
+import java.util.Scanner;
+
 public class RunnerClass {
     public static void main(String[] args) {
+        // Java Array 
+        int size;
+        Scanner scanner = new Scanner(System.in);
+        size = scanner.nextInt();
+        scanner.nextLine();
+        
+        // Array Creation and Declaration
+        int[] myArray = new int[size];
+        for (int i = 0; i < size; i++) {
+            myArray[i] = scanner.nextInt();
+        }
+        
+        System.out.println("\n===========================================");
+        
+        // Print Array
+        for (int i = 0; i < myArray.length; i++) {
+            System.out.print(myArray[i]+" ");
+        }
+        
+        
+        
+        
         
     }
 }
