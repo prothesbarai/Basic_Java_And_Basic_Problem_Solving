12591eab49ca051eceeec974b118dbe10cdba802
prothesbarai <aspprothesbarai1619@gmail.com>
2025-01-12 11:00:38 +0600

    kk

--- basicjava/this_keyword/RunnerClass.java
+++ basicjava/this_keyword/RunnerClass.java
@@ -5,24 +5,34 @@ import java.util.Scanner;
 public class RunnerClass {
     public static void main(String[] args) {
         // Java Array 
-        int col,row,num;
+        int col,row;
         Scanner scanner = new Scanner(System.in);
-        //col = scanner.nextInt();
-        //row = scanner.nextInt();
-        num = scanner.nextInt();
+        System.out.print("Row : ");
+        row = scanner.nextInt();
+        System.out.print("Col : ");
+        col = scanner.nextInt();
         scanner.nextLine();
         
-        String[] myArray = {"Days : ","Sunday","Monday","Tuesday","Wednesday","Thusday","Friday","Saturday"};
+        int[][] myArray = new int[row][col];
         
-        
-        if (num > 0 && num <=7) {
-            String getData = myArray[num];
-            System.out.println("Day is : "+getData);
-        }else{
-            System.out.println("Oh ! NO ! Input is Wrong !");
+        // Input....
+        for (int i = 0; i < row; i++) {
+            for (int j = 0; j < col; j++) {
+                myArray[i][j] = scanner.nextInt();
+            }
         }
         
+        System.out.println("\n\n===================================================\n\n");
         
+        // Output .....
+        for (int i = 0; i < row; i++) {
+            
+            for (int j = 0; j < col; j++) {
+                System.out.print(" "+myArray[i][j]);
+            }
+            
+            System.out.println();
+        }
         
         
         
