c3e4c2a7bb544af4c68d00dbd88864472c0c2dc0
prothesbarai <aspprothesbarai1619@gmail.com>
2025-01-15 11:33:16 +0600

    ll

--- basicjava/this_keyword/RunnerClass.java
+++ basicjava/this_keyword/RunnerClass.java
@@ -8,59 +8,5 @@ public class RunnerClass {
         
         
         
-        int rows, cols, sum = 0;
-        Scanner scanner = new Scanner(System.in);
-        System.out.print("Number Of Row : ");
-        rows = scanner.nextInt();
-        System.out.print("Number Of Column : ");
-        cols = scanner.nextInt();
-        scanner.nextLine();
-
-        // Array Creation And Declaration
-        int[][] myArray = new int[rows][cols];
-
-        // Input From User
-        for (int i = 0; i < rows; i++) {
-            for (int j = 0; j < cols; j++) {
-                System.out.printf("myArray[%d][%d] = ", i, j);
-                myArray[i][j] = scanner.nextInt();
-            }
-        }
-
-        System.out.println("\n\n==================================\n\n");
-
-        System.out.println("Output : \n");
-        // Output
-        for (int i = 0; i < rows; i++) {
-            for (int j = 0; j < cols; j++) {
-                System.out.print(myArray[i][j] + " ");
-            }
-            System.out.println();
-        }
-
-        
-        // Diagonal Triangle Summations
-        System.out.println("Printed Sum of Diagonal Ray : ");
-        int sumOfDiagonal = 0,sumOfUpperTrinangle = 0,sumOfLowerTriangle = 0;
-        for (int i = 0; i < rows; i++) {
-            for (int j = 0; j < cols; j++) {
-                if (i == j) {
-                    sumOfDiagonal += myArray[i][j];
-                }
-                if (i<j) {
-                    sumOfUpperTrinangle += myArray[i][j];
-                }
-                if (i>j) {
-                    sumOfLowerTriangle += myArray[i][j];
-                }
-            }
-        }
-        System.out.println("Sum Of Diagonal : "+sumOfDiagonal);
-        System.out.println("Sum Of Upper Triangle : "+sumOfUpperTrinangle);
-        System.out.println("Sum Of Lower Triangle : "+sumOfLowerTriangle);
-
-        
-        
-        
     }
 }
