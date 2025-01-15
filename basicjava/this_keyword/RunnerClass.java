dc7d13c24f6cf9c1686a6a4eb96279d4eedeb26f
prothesbarai <aspprothesbarai1619@gmail.com>
2025-01-15 11:57:07 +0600

    v3

--- basicjava/this_keyword/RunnerClass.java
+++ basicjava/this_keyword/RunnerClass.java
@@ -16,7 +16,7 @@ public class RunnerClass {
         // Input in two dimentional array
         for (int i = 0; i < rows; i++) {
             for (int j = 0; j < cols; j++) {
-                System.out.printf("\n\nmyArray[%d][%d] = ",i,j);
+                System.out.printf("\nmyArray[%d][%d] = ",i,j);
                 myArray[i][j] = scanner.nextInt();
             }
         }
@@ -26,6 +26,7 @@ public class RunnerClass {
             for (int j = 0; j < cols; j++) {
                 System.out.print(myArray[i][j]+" ");
             }
+            System.out.println();
         }
         //Sum Of All Items in This Array
         int sum = 0;
@@ -34,7 +35,25 @@ public class RunnerClass {
                 sum += myArray[i][j];
             }
         }
-        System.out.println("Sum Of All Values : "+sum);
+        System.out.println("\nSum Of All Values : "+sum);
+        // Sum Of All values Diagonally
+        int sumOfDiagonal = 0, sumOfUpperTriangle = 0, sumOfLowerTriangle = 0;
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                if (i==j) {
+                    sumOfDiagonal += myArray[i][j];
+                }
+                if (i<j) {
+                    sumOfUpperTriangle += myArray[i][j];
+                }
+                if (i>j) {
+                    sumOfLowerTriangle += myArray[i][j];
+                }
+            }
+        }
+        System.out.println("\nSum Diagonal : "+sumOfDiagonal);
+        System.out.println("Sum Of Upper Triangle : "+sumOfUpperTriangle);
+        System.out.println("Sum Of Lower Triangle : "+sumOfLowerTriangle);
         
     }
 }
