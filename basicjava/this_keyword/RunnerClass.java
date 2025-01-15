0a6e4c31d16c159fd3d5a5b48d5661b7664cd951
prothesbarai <aspprothesbarai1619@gmail.com>
2025-01-15 11:49:19 +0600

    v22

--- basicjava/this_keyword/RunnerClass.java
+++ basicjava/this_keyword/RunnerClass.java
@@ -16,16 +16,25 @@ public class RunnerClass {
         // Input in two dimentional array
         for (int i = 0; i < rows; i++) {
             for (int j = 0; j < cols; j++) {
-                System.out.printf("myArray[%d][%d] = ",i,j);
+                System.out.printf("\n\nmyArray[%d][%d] = ",i,j);
                 myArray[i][j] = scanner.nextInt();
             }
         }
         // Output in Two dimentional array
+        System.out.println("\n");
         for (int i = 0; i < rows; i++) {
             for (int j = 0; j < cols; j++) {
-                System.out.println(myArray[i][j]+" ");
+                System.out.print(myArray[i][j]+" ");
             }
         }
+        //Sum Of All Items in This Array
+        int sum = 0;
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                sum += myArray[i][j];
+            }
+        }
+        System.out.println("Sum Of All Values : "+sum);
         
     }
 }
