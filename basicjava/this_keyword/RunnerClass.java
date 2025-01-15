83b890cc3dead8cfd05fa4ef4d37f47c24d86f10
prothesbarai <aspprothesbarai1619@gmail.com>
2025-01-15 11:42:22 +0600

    v3

--- basicjava/this_keyword/RunnerClass.java
+++ basicjava/this_keyword/RunnerClass.java
@@ -5,8 +5,27 @@ import java.util.Scanner;
 public class RunnerClass {
 
     public static void main(String[] args) {
-        
-        
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Num Of Rows : ");
+        int rows = scanner.nextInt();
+        System.out.print("Num Of Cols : ");
+        int cols = scanner.nextInt();
+        scanner.nextLine();
+        // Creation and Declaration An two dimentional Array
+        int[][] myArray = new int[rows][cols];
+        // Input in two dimentional array
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                System.out.printf("myArray[%d][%d] = ",i,j);
+                myArray[i][j] = scanner.nextInt();
+            }
+        }
+        // Output in Two dimentional array
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                System.out.println(myArray[i][j]+" ");
+            }
+        }
         
     }
 }
