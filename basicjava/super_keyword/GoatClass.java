e10443e21fff91bc407b7c1e539606994b998efd
prothesbarai <aspprothesbarai1619@gmail.com>
2025-01-16 22:36:33 +0600

    Super Keywords

--- basicjava/super_keyword/GoatClass.java
+++
@@ -1,7 +0,0 @@
-package com.mycompany.basicjava.super_keyword;
-public class GoatClass extends AnimalClass{
-    public GoatClass(String names,String colors, int legs, double weight, boolean wool) {
-        super(names,colors, legs, weight, wool);
-    }
-    
-}
